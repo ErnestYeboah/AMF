74f2e6925c8904321deb440a17408befe1601e45
Ernest Yeboah <yeboahernest246@gmail.com>
2025-09-24 20:30:19 +0000

    deploying the app

--- backend/api_store/api_store/settings.py
+++ backend/api_store/api_store/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
      "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -57,7 +58,9 @@ MIDDLEWARE = [
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://amaetonfashionhouse.vercel.app"
+
    
 ]
 
