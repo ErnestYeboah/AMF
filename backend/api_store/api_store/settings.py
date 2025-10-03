7431e3ba2e6b7c4fa19f4306f9a97671853ef02b
Ernest Yeboah <yeboahernest246@gmail.com>
2025-10-03 20:56:49 +0000

    solving the refresh state

--- backend/api_store/api_store/settings.py
+++ backend/api_store/api_store/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^1lp%2a*r#$xp14nl)2$&3+v+gcr(r7^n9dqk7camv+cvb@(#n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://amaetonfashionhouse.vercel.app']
 
 
 # Application definition
